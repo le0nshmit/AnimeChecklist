2edb3b85f27abe6bd8590fe24fb0abf87fd17481
le0nshmit <leonpickard6@gmail.com>
2026-01-03 17:26:16 +0000

    Main Menu

---
+++ src/anime_checklist/main.py
@@ -0,0 +1,10 @@
+
+def main() -> None:
+    print("Anime Check List\n\n")
+    while True:
+        print("1) View Anime List\n2) Modify Anime List")
+        command = input("> ")
+
+if __name__  == '__main__': main()
+
+
