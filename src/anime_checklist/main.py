ce3f608970216d8d7a553b0e7ef7281a5b81abfd
le0nshmit <leonpickard6@gmail.com>
2026-01-03 22:23:34 +0000

    Main - main menu complete

--- src/anime_checklist/main.py
+++ src/anime_checklist/main.py
@@ -1,9 +1,22 @@
+from anime_checklist.utils import menu_validation, clear
+from anime_checklist.view import list_anime
+from anime_checklist.modify import modify_main
+import sys
+
+MAIN_MENU = {
+    '1' : list_anime,
+    '2' : lambda: modify_main(clear),
+    'exit': sys.exit,
+}
+
 
 def main() -> None:
-    print("Anime Check List\n\n")
+    clear()
+    print("\n\n[> Anime Check List <]\n\n")
     while True:
-        print("1) View Anime List\n2) Modify Anime List")
+        print("[> 1) View Anime List <]\n[> 2) Modify Anime List <]")
         command = input("> ")
+        menu_validation(command, MAIN_MENU)
 
 if __name__  == '__main__': main()
 
